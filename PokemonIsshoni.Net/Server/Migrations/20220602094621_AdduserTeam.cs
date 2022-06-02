a4da31a58f85eca1cfa256cb0a08a26e43b1fc83
ssccinng <whs11998@163.com>
2022-06-02 17:48:50 +0800

    mig

---
+++ PokemonIsshoni.Net/Server/Migrations/20220602094621_AdduserTeam.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonIsshoni.Net.Server.Migrations
+{
+    public partial class AdduserTeam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PCLUserTeams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(270)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PokeTeamId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCLUserTeams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PCLUserTeams_PCLPokeTeams_PokeTeamId",
+                        column: x => x.PokeTeamId,
+                        principalTable: "PCLPokeTeams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PCLUserTeams_PokeTeamId",
+                table: "PCLUserTeams",
+                column: "PokeTeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PCLUserTeams");
+        }
+    }
+}
