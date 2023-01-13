1aff3e19c1635eb7e76d7a197993c68d317effc6
scixing <whs11998@163.com>
2023-01-13 14:33:46 +0800

    111

---
+++ PokemonIsshoni.Net/Client/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PokemonIsshoni.Net.Client;
+using PokemonIsshoni.Net.Client.Factory;
+using PokemonIsshoni.Net.Client.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("PokemonIsshoni.Net.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("PokemonIsshoni.Net.ServerAPI"));
+// ��������
+builder.Services.AddHttpClient("PokemonIsshoni.Net.ServerAPI.Anonymous", client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+#region �Զ������
+builder.Services.AddScoped<UserInfoServices>();
+builder.Services.AddScoped<PCLServices>();
+#endregion
+
+builder.Services.AddApiAuthorization()
+    .AddAccountClaimsPrincipalFactory<CustomUserFactory>();
+builder.Services.AddMudServices();
+await builder.Build().RunAsync();
+
+
