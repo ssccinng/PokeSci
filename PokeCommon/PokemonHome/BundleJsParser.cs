8d553487bc3fcc46fa06c263db4aac2d209b578a
scixing <whs11998@163.com>
2023-11-26 01:11:50 +0800

    update

--- PokeCommon/PokemonHome/BundleJsParser.cs
+++ PokeCommon/PokemonHome/BundleJsParser.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PokeCommon.PokemonHome
 {
-    internal class BundleJsParser
+    public class BundleJsParser
     {
+        public static readonly string BundleUrl = "https://resource.pokemon-home.com/battledata/js/bundle.js";
+
+        public BundleData ParseAsync(string data)
+        {
+            Regex regex3 = new Regex(@"this.dex = (((?'Open'\{)[^\{\}]*)+((?'Close-Open'\})[^\{\}]*)+)*(?(Open)(?!))");
+
+            return new BundleData();
+        }
+    }
+
+    public class BundleData
+    {
+
     }
 }
---
+++ PokeCommon/PokemonShowdownTools/PSUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PokeCommon.PokemonShowdownTools
+{
+    public static class PSUtils
+    {
+        public static string GetPurePSId(string id) => Regex.Replace(id, @"[^A-Za-z0-9]", "").ToLower();
+    }
+}
