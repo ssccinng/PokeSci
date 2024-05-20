66159b2434612de2adbee33ca3b81b1c7e5febf5
scixing <whs11998@163.com>
2024-05-20 09:41:34 +0800

    更新

---
+++ PokeTeamAI/IAITeamBuilder.cs
@@ -0,0 +1,15 @@
+﻿using PokeCommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeTeamAI
+{
+    public interface IAITeamBuilder
+    {
+        // 负责把已知信息传入AI，AI根据已知信息进行预测
+        SimpleGamePokemonTeam PredictTeam(SimpleGamePokemonTeam team);
+    }
+}
