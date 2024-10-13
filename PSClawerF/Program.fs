898050c50417285346477ce22d80e5a4477fb10b
scixing <whs11998@163.com>
2024-10-13 08:01:05 +0800

    update

---
+++ PSClawerF/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open PokePSCore
+let psClient = PSClient ("testt", "")
+
+let getRuleRoomAsync rule = 
+    let room = psClient.GetRoomListAsync ()
+    printfn "%A" room
+
+while true do
+    let result = psClient.GetPokemonAsync("pikachu").Result
+    printfn "%A" result
+    System.Threading.Thread.Sleep(1000)
