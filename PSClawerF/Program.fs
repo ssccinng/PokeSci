ecea59ebc0498c42fa904783a3c6741b48e751dc
scixing <whs11998@163.com>
2024-10-20 23:09:19 +0800

    更新

--- PSClawerF/Program.fs
+++ PSClawerF/Program.fs
@@ -1,6 +1,8 @@
 ﻿open PsRoomListener.ListenBot
 open System
 
+
+
 let rules = [|  "gen9vgc2024regh";"gen9vgc2024reghbo3";"gen9vgc2024regg";"gen9vgc2024reggbo3" |]
 
 let bot = Bot("gen9vgc2024regh", None)
@@ -8,3 +10,4 @@ let bot = Bot("gen9vgc2024regh", None)
 bot.initRoomListener()
 
 Console.ReadLine() |> ignore
+
--- PsRoomListener/Library.fs
+++ PsRoomListener/Library.fs
@@ -2,9 +2,11 @@
 
 open PokePSCore
 open PSReplayParser
+open PSReplayParser.PSReplayParser
 
 module ListenBot = 
-    type Bot(rule: string, score: option<int>) =
+    open System.Collections.Generic
+    type Bot(rule: string, score: int option) =
         let psClient = PSClient ("testt", "")
         let mutable roomSet = set []
 
@@ -63,5 +65,81 @@ module ListenBot =
                     // Process `e` if needed
                     ()
             )
+
+            do loopToSendGetRoom() |> Async.Start
+
+
+    type MuitBot(rules: string array, score: int option) =
+        let psClient = PSClient ("testt", "")
+
+        let mutable roomSetDict = Dictionary<string, Set<string>>()
+
+        do for rule in rules do
+            roomSetDict.Add(rule, set [])
+
+        let mutable nowRule = ""
+
+        let connectAndLoginAsync() =
+            async {
+                do! psClient.ConnectAsync() |> Async.AwaitTask
+                let! _ = psClient.LoginGuestAsync() |> Async.AwaitTask
+                printfn "Connected and logged in successfully!"
+            }
+        
+
+        do connectAndLoginAsync() |> Async.RunSynchronously
+
+        let loopToSendGetRoom() =
+            async {
+                let scoreLimit = 
+                    match score with
+                        | Some s -> s
+                        | None -> -1
+                while true do
+
+                    for rule in rules do
+                        nowRule <- rule
+                        do! psClient.GetRoomListAsync(rule, scoreLimit)
+                            |> Async.AwaitTask
+
+                        do! Async.Sleep 10000
+                    
+            }
+        member val event = new Event<PsReplayInfo>()
+
+        member this.initRoomListener() =
+            psClient.add_OnGetRoomList (
+                fun e -> 
+                    async {
+                        let newRoomSet = 
+                            e.rooms.Keys 
+                            |> Seq.map string
+                            |> set
+
+                        let diff = 
+                            newRoomSet |> Set.difference roomSetDict.[nowRule]
+                        roomSetDict.[nowRule] <- newRoomSet
+
+                        for key in diff do
+                            do! psClient.SendJoinAsync key |> Async.AwaitTask
+
+                            // Process `res` if needed
+                            do! Async.Sleep 1000 // Replace Thread.Sleep with Async.Sleep to avoid blocking
+                            do! psClient.SendLeaveAsync key |> Async.AwaitTask
+                            do! Async.Sleep 1000
+
+                    }
+                    |> Async.Start // Start the async operation non-blocking
+            )
+
+            psClient.add_BattleRoomData(
+                fun e -> 
+                    printfn "BattleRoomData: %A" e
+                    //printfn "BattleRoomData: %A" ()
+                    // Process `e` if needed
+                    e  |> PSReplayParser.Parser |> this.event.Trigger
+            )
+
+            
 
             do loopToSendGetRoom() |> Async.Start
